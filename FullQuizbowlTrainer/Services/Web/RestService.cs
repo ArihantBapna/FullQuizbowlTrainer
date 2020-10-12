e923871a5a6b6f4a37dbd3518ced71cd32ba8b1a
ArihantBapna <greatbapna@gmail.com>
2020-10-12 09:29:27 -0400

    Rest interface

---
+++ FullQuizbowlTrainer/Services/Web/RestService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FullQuizbowlTrainer.Services.Web
+{
+    public interface IRestService
+    {
+        Task<string> Get(string uri);
+    }
+
+    public class RestService : IRestService
+    {
+        private const string BaseUrL = "https://power15-restful.herokuapp.com";
+
+        HttpClient httpClient;
+
+        public RestService()
+        {
+            httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(BaseUrL);
+        }
+
+        public async Task<string> Get(string uri)
+        {
+            var response = await httpClient.GetAsync(uri);
+            response.EnsureSuccessStatusCode();
+            return response.Content.ReadAsStringAsync().Result;
+        }
+    }
+}
